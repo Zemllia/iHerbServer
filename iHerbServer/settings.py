54e2648d3d669e881ba7cf2450832357de46daa2
esheloncr <igoninalexeynn@gmail.com>
2021-06-19 09:10:52 +0300

    fix commit

--- iHerbServer/settings.py
+++ iHerbServer/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'iherb_db',
         'USER': 'postgres',
-        'PASSWORD': 'ghjujyland',
+        'PASSWORD': 'b4hxd6u7',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
